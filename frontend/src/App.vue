e3838b2c6fea11240a9121fbca0b2cf66825e6dd
Matt Clyde <me@mjclyde.com>
2024-08-27 19:16:20 -0600

    added frontend vue app

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <h1 class="text-3xl font-bold">Hello There</h1>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+</style>
